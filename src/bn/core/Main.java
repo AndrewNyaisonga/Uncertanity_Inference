e36605c803c85081ed9ccaba64e2f99d0376ed6d
anyaison <anyaison@u.rochester.edu>
2018-04-03 12:16:02 -0400

    Update some more

--- src/bn/core/Main.java
+++ src/bn/core/Main.java
@@ -25,7 +25,7 @@ public class Main {
 
 
 		String lang = args[0];			// Language
-		String inference = args[1];	// Take the value of Inference
+		String inference = args[1];		// Take the value of Inference
 		String[] parameters;
 		String filename = "";
 
@@ -45,29 +45,27 @@ public class Main {
 		case "MyBNApproxInferencer":
 			System.out.println("Approximation Inference!");
 			samples = Integer.parseInt(args[2]); // Get sample
-
-			System.out.println("Number of samples: " + samples + "."); 
 			filename = args[3];		//XML or BIF file
 			parameters = Arrays.copyOfRange(args, 4, args.length);
+			System.out.println("Sample: " + samples); 
 			break;
 //		case "MyBNGibbsInferencer":
 //			System.out.println("Gibbs Inference!");
 //			samples = Integer.parseInt(args[2]); // Get sample
 //
-//			System.out.println("Number of samples: " + samples + "."); 
 //			filename = args[3];		//XML or BIF file
 //			parameters = Arrays.copyOfRange(args, 4, args.length);
+//			System.out.println("Sample: " + samples); 
 //			break;
 
 		default:
-			System.out.println("Not a valid selection!");
+			System.out.println("We only support MyBNInferencer,MyBNApproxInferencer and MyBNGibbsInferencer");
 			return;
 
 		}
 		
 		
 		String type = "XML";
-		// Files
 		if (filename.toLowerCase().endsWith(".bif")) { 
 			System.out.println("BIF file."); type = "BIF";
 		}
@@ -75,38 +73,34 @@ public class Main {
 			System.out.println("XML file."); type = "XML";
 		}
 		else { 
-			System.out.println("ERROR!");  
+			System.out.println("We only support .XML and .BIF files!");  
 			return;
 		}
-
-		// Parameters
+		
 		System.out.println("\n\nParameters:");
+		String[] evidenceRandomVariable = new String[parameters.length/2]; 	// 5 parameters means 2 are evidence,
+		Object[] evidenceValues = new Object[parameters.length/2]; 	// values (2);
 		
-		String[] evidenceVarNames = new String[parameters.length/2]; 	// If 5 parameters, 2 are evidence,
-		Object[] evidenceValues = new Object[parameters.length/2]; 	// and 2 are values
-		
-		int evIndex = 0;
+		int index = 0;
 		for(int i = 0; i < parameters.length; i++) {
 			String P = parameters[i];
-			System.out.print(P + " ");
-			
+			System.out.print(P + " ");		
 			if (i == 0) continue; 	// Skip ahead after query
-			
 			if (i % 2 == 1) { 				// Every other is a variable name
 				//System.out.print("<< ");
-				evidenceVarNames[evIndex] = P;
-				evIndex ++;
+				evidenceRandomVariable[index] = P;
+				index ++;
 			} else {						// The next item is always the value object
-				evidenceValues[evIndex-1] = P;
+				evidenceValues[index-1] = P;
 			}
 		}
 		
 		System.out.println("\n\nEvidence: ");
-		for (String e: evidenceVarNames) System.out.print(e + "\t"); System.out.println("");
+		for (String e: evidenceRandomVariable) System.out.print(e + "\t"); System.out.println("");
 		for (Object e: evidenceValues) System.out.print(e + "\t");
 
-		String queryVarName = parameters[0];
-		System.out.println("\n\nPrinting distribution for: " + queryVarName);
+		String queryRandomVariable = parameters[0];
+		System.out.println("\n\nPrinting distribution for: " + queryRandomVariable);
 		
 		/* Time to actually do things! */
 		try {
@@ -126,26 +120,25 @@ public class Main {
 				BN = parser.readNetworkFromFile(newpath);
 			}
 			
-			RandomVariable query = BN.getVariableByName(queryVarName);
+			RandomVariable query = BN.getVariableByName(queryRandomVariable);
 			
 			Assignment A = new Assignment();
 			RandomVariable[] evidence = new RandomVariable[evidenceValues.length];
-			for (int i = 0; i < evidence.length; i++) {		// Get RandomVariables for each evidence value
-				evidence[i] = BN.getVariableByName(evidenceVarNames[i]);
-				A.set(evidence[i], evidenceValues[i]);		// Add the evidence var/val to the set of assignments 
+			for (int i = 0; i < evidence.length; i++) {	
+				evidence[i] = BN.getVariableByName(evidenceRandomVariable[i]);
+				A.set(evidence[i], evidenceValues[i]);		// evidence are added to assignment
 			}
 			
 			
-			///BN.print(System.out);
 			if (inference.equals("MyBNInferencer")) {
 				System.out.println("\n\nEnumeration Inferencing.... \n");
 				ExactInference inf = new ExactInference(BN);
 				
 				final long startTime = System.currentTimeMillis();
-				Distribution dist = inf.ENUMERATIONASK(query,A, BN);
+				Distribution dist = inf.ENUMERATIONASK(query,A, BN); //ENUMERATIONASK(RandomVariable X, Assignment e, BayesianNetwork bn)
 				final long endTime = System.currentTimeMillis();
 				
-				System.out.println("Completed in " + (endTime-startTime) + " ms.");
+				System.out.println("Completed in " + (endTime-startTime) + " ms.");  //Ferguson suggestion of keeping track of time
 				System.out.println("\n\nProbabilities:" + dist.toString());
 			}
 			
@@ -153,12 +146,12 @@ public class Main {
 				System.out.println("\n\nRejection Inferencing.... \n");
 				RejectionInference inf = new RejectionInference();
 				
-				//RejectAsk(RandomVariable X, BayesianNetwork bn, Assignment e, int N)
+				
 				final long startTime = System.currentTimeMillis();
-				Distribution dist = inf.RejectAsk(query,BN,A, samples);
+				Distribution dist = inf.RejectAsk(query,BN,A, samples); //RejectAsk(RandomVariable X, BayesianNetwork bn, Assignment e, int N)
 				final long endTime = System.currentTimeMillis();
 				
-				System.out.println("\nCompleted in " + (endTime-startTime) + " ms.");
+				System.out.println("\nCompleted in " + (endTime-startTime) + " ms."); //Ferguson suggestion of keeping track of time
 				System.out.println("\n\nProbabilities:" + dist.toString());
 			}
 			
